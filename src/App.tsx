89cc7f2c80664f85a4958da875310466251d8273
Italo Santos <italosantoscont@gmail.com>
2025-12-18 17:25:29 -0300

    react Icon

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ export function App() {
           
           <div className="d-flex justify-content-center align-items-center gap-3 mb-4">
             <h1 className=" m-0 text-dark">Agenda em React</h1>
-            <img width="45" height="45" src="/icons8-react-native.gif" alt="React"
+            <img width="45" height="45" src="https://img.icons8.com/?size=80&id=t5K2CR8feVdX&format=gif" alt="React"
             />
           </div>
           <Link to="/contatos"className="btn btn-dark btn-lg px-4">Listar contatos</Link>
